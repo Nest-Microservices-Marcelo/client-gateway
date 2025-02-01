1097f3f533949f45072409448f9e3b40da8ea008
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-02-01 09:02:12 -0300

    first commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,83 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { error } from 'console';
+import { catchError, firstValueFrom } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PRODUCT_SERVICE } from 'src/config';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send(
+      { cmd: 'create_product' },
+      createProductDto,
+    );
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      { cmd: 'find_all_products' },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    //es lo mismo que el try catch de abajo
+    //el método pipe() se usa para encadenar operadores en un observable, permitiendo transformar, filtrar, o manipular los valores emitidos antes de que sean recibidos por un suscriptor.
+    return this.productsClient.send({ cmd: 'find_one_product' }, { id }).pipe(
+      catchError((err) => {
+        throw new RpcException(err);
+      }),
+    );
+
+    // try {
+    //   const product = await firstValueFrom(
+    //     //Esto es un observable, por lo que se debe esperar a que se resuelva
+    //     this.productsClient.send({ cmd: 'find_one_product' }, { id }),
+    //   );
+    //   return product;
+    // } catch (error) {
+    //   throw new RpcException(error);
+    // }
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productsClient.send({ cmd: 'delete_product' }, { id });
+  }
+
+  @Patch(':id')
+  patchProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    //Se envia el updateProductDto con el spread operator para pasar todos los valores inclusive el id, como espera el updateProductDto de products-ms
+    return this.productsClient
+      .send({ cmd: 'update_product' }, { id, ...updateProductDto })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+}
