c94bb613717cc10e30247a6b7f57416aee665de2
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-04-07 21:12:12 -0300

    add docker

--- README.md
+++ README.md
@@ -8,8 +8,14 @@ Es el encargado de recibir las peticiones, enviarlas a los servicios correspondi
 1. Clonar el repositorio
 2. Instalar dependencias
 3. Crear un archivo `.env` basado en el `env.template`
-4. Tener levantados lo microservicios que se van a consumir
-5. levantar el proyecto con `npm run start:dev`
+4. Levantar el servidor de NATS
+
+```
+docker run -d --name nats-main -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
+```
+
+5. Tener levantados lo microservicios que se van a consumir
+6. levantar el proyecto con `npm run start:dev`
 
 ## Nats
 
--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -13,6 +13,16 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
     //Se obtiene el error de la excepcion
     const rpcError = exception.getError();
 
+    //Se envia un mensaje personalizado, en este caso si el error es un Empty response devolvemos un 500
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(500).json({
+        status: 500,
+        message: rpcError
+          .toString()
+          .substring(0, rpcError.toString().indexOf('(') - 1),
+      });
+    }
+
     if (
       typeof rpcError === 'object' &&
       'status' in rpcError &&
